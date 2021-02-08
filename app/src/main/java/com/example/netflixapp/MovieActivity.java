a82a0f72db802798f98f97bcdc558f078d63bdbb
Eric <eric.degomar@gmail.com>
2021-02-08 07:58:10 -0300

    incluido a pagina com toolbar, app quebra ao tentar rodar, sem erros do console

---
+++ app/src/main/java/com/example/netflixapp/MovieActivity.java
@@ -0,0 +1,25 @@
+package com.example.netflixapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+
+public class MovieActivity extends AppCompatActivity {
+
+    androidx.appcompat.widget.Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.arrow_back);
+
+    }
+}
